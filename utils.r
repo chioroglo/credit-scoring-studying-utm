fdff74e1fd8bb8e06ffe8fba5acb8cb96d1619f1
chioroglo <kioroglomobster@gmail.com>
2023-10-30 00:12:18 +0200

    remove unused util

--- utils.r
+++ utils.r
@@ -5,14 +5,3 @@ parse_decimal_or_na <- function(string) {
   }
   return(numeric_value)
 }
-
-replace_outliers_with_mode <- function(data, group_column, column_name, quantile_value) {
-  q_value <- quantile(data[[column_name]], quantile_value, na.rm = TRUE)
-  
-  data <- data %>%
-    group_by({{ group_column }}) %>%
-    mutate({{ column_name }} := ifelse({{ column_name }} > q_value, Mode({{ column_name }}), {{ column_name }})) %>%
-    ungroup()
-  
-  return(data)
-}
